ec6af4e6ee5d0d8aefc48c950352a0c082f7bcf8
SOUFIANEZAZA <72149789+SOUFIANEZAZA@users.noreply.github.com>
2020-12-19 03:04:07 +0100

    Add files via upload

---
+++ FunctionData/USER_DATABase.py
@@ -0,0 +1,25 @@
+USERID_FOR_MASSLOOKER = [
+"mlkellyyy",
+    "tabbyknightt",
+    "siddhantrao007",
+    "camilla_akerberg",
+    "v.chakir",
+    "positive_card",
+    "jamiedorego_ifbbpro",
+    "_runningmama_",
+    "happy4life_11",
+    "muscleandfitness",
+    ]
+
+USERID_FOR_INSHACKLE = [
+"ulissesworld",
+    "andreideiu_",
+    "sergiconstance",
+    "jeremy_buendia",
+    "simeonpanda",
+    "sadikhadzovic",
+    "kaigreene",
+    "philheath",
+    "big_ramy",
+    "ronniecoleman8",
+    ]
